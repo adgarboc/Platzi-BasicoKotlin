61a0e751dc145052f68a86afef3dcbb1d641d44b
Adán García <adgarboc@gmail.com>
2020-07-27 10:20:38 -0500

    Clase 4

---
+++ HolaMundo/src/HolaMundo.kt
@@ -0,0 +1,19 @@
+fun main(args: Array<String>) {
+    println("Hola Mundo")
+
+    println("==Como tipos primitivos==")
+    println(1+1)
+    println(3-1)
+    println(2*2)
+    println(4/2)
+    println(7%2)
+
+    println("==Como objetos==")
+    val a = 4
+    val b = 2
+
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+}
